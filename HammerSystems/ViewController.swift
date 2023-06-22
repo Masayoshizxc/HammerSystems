8ff27c66417ec4d416fd3bff6a99257a1f77017c
Adilet <masayoshizxc@gmail.com>
2023-06-22 21:51:36 +0600

    Start

---
+++ HammerSystems/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HammerSystems
+//
+//  Created by Adilet on 22/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+}
